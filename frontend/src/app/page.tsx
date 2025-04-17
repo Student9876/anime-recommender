44a71b4ca60f4ef716fc40d43606b33d31f3a924
student9876 <shouvik9876@gmail.com>
2025-04-17 20:15:21 +0530

    .

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-	return <div>
-              <h1 className="text-[20px]">Hello World!</h1>
-  </div>;
+	return (
+		<div>
+			<h1 className="text-[20px]">Hello World!</h1>
+		</div>
+	);
 }
